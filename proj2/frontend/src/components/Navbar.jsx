7ad295c66fef4c7912d7ee92ce2511a96c09e3ba
Om Kumar Singh <Sparkywarder@MacBookAir.lan>
2025-11-03 22:50:19 -0500

    updated styling

--- proj2/frontend/src/components/Navbar.jsx
+++ proj2/frontend/src/components/Navbar.jsx
@@ -14,10 +14,10 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <Link to="/" className="font-semibold hover:underline">Home</Link>
-        <Link to="/broadcast" className="font-semibold hover:underline">Broadcast</Link>
-        <Link to="/your-runs" className="font-semibold hover:underline">Your Runs</Link>
-        <Link to="/profile" className="font-semibold hover:underline">Profile</Link>
+        <Link to="/">Home</Link>
+        <Link to="/broadcast">Broadcast</Link>
+        <Link to="/your-runs">Your Runs</Link>
+        <Link to="/profile">Profile</Link>
       </div>
 
       <div className="navbar-right">
